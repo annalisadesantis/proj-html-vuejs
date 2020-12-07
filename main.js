6bda2694354c3059ed109650675af8fc184e6147
Annalisa <annalisadesantis770@gmail.com>
2020-12-07 11:02:05 +0100

    cambio chiavi in oggetti in vue

--- main.js
+++ main.js
@@ -2,25 +2,48 @@ var app = new Vue({
     el: "#root",
     data: {
         // HEADER
-        logo_header: "immagini/logo-mobile.png",
-        logo_name: "logo-avada",
-        menu: ["Home", "Services", "About", "Videos", "Blog", "Store"],
-        button_header: "Schedule a workout",
-    
+        header: {
+            logo_header: "immagini/logo-mobile.png",
+            logo_name: "logo-avada",
+            menu: ["Home", "Services", "About", "Videos", "Blog", "Store"],
+            button_header: "Schedule a workout"
+        },
+
         // MENU A TENDINA
         tendina: false,
 
-
         // FOOTER
-        logo_footer: "immagini/logo.png",
-        text_footer: "Ac aliquam neque sagittis diam faucibus vitae purus turpis phasellus. Pellentesque consectetur amet purus ultrices mauris.",
-        icon_social: ["fa-facebook-f", "fa-twitter", "fa-youtube", "fa-instagram", "fa-linkedin-in"],
-        title_box: "Recent Posts",
-        links_footer: ["The best protein shake", "Ultimate cardio workout", "New juices available now", "Tips to find training partners", "20 best healthy recipes"],
-        contact_footer: "Contact Informations",
-        address_footer: "4746 Tipple Road Michigan 48449",
-        general_footer: ["Mobile : 1.800.000.0000", "Email : info@your-company.com"]
-
+        footer:{
+            logo_footer: "immagini/logo.png",
+            text_footer: "Ac aliquam neque sagittis diam faucibus vitae purus turpis phasellus. Pellentesque consectetur amet purus ultrices mauris.",
+            icon_social: [
+                {
+                    icon: "fa-facebook-f",
+                    link: "#"
+                },
+                {
+                    icon: "fa-twitter",
+                    link: "#"
+                },
+                {
+                    icon: "fa-youtube",
+                    link: "#"
+                },
+                {
+                    icon: "fa-instagram",
+                    link: "#"
+                },
+                {
+                    icon: "fa-linkedin-in",
+                    link: "#"
+                }
+            ],
+            title_box: "Recent Posts",
+            links_footer: ["The best protein shake", "Ultimate cardio workout", "New juices available now", "Tips to find training partners", "20 best healthy recipes"],
+            contact_footer: "Contact Informations",
+            address_footer: "4746 Tipple Road Michigan 48449",
+            general_footer: ["Mobile : 1.800.000.0000", "Email : info@your-company.com"]
+        }
     },
     methods:{
         // Show menu tendina
